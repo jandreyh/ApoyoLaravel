92739c1a49f1d930ac5deed6fd78a7ed439adf66
Jeisson Martinez <jeissonandrey.martinezh@gmail.com>
2022-08-03 11:28:35 -0500

    Repositorio con todo el apoyo de los cursos y algunos cambios de direccion

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $users = User::latest()->get();
+
+        return view('users.index', [
+            'users' => $users
+        ]);
+    }
+
+    public function store(Request $request){
+        $this->validate($request, [
+            'name' => 'required|max:255',
+            'email' => 'required|email|max:255|unique:users',
+            'password' => 'required|min:6|confirmed',
+        ]);
+        return back();
+
+    }
+    public function destroy(User $user){
+        $user->delete();
+
+        return back();
+
+    }
+}
+
---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel 8</title>
+    <!-- Bootstrap 5 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <!-- CDN DataTable con Bootstrap 5 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous" />
+    <link href="https://cdn.datatables.net/1.10.24/css/dataTables.bootstrap5.min.css" rel="stylesheet" crossorigin="anonymous" />
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="card">
+            <div class="card-header">Listado de usuarios</div>
+            <div class="card-body">
+                <div class="datatable table-responsive">
+                    <div>
+                        <div class="card">
+                            <form action="{{ route('users.store') }}" method="POST"></form>
+                            <div class="form-row">
+                                <div class="col-sm-3">
+                                    <input type="text" name="name" class="form-control" placeholder="Nombre">
+                                </div>
+                                <div class="col-sm-4">
+                                    <input type="text" name="email" class="form-control" placeholder="Email">
+                                </div>
+                                <div class="col-sm-3">
+                                    <input type="password" name="password" class="form-control" placeholder="Contraseña">
+                                </div>
+                                <div class="col-auto">
+                                    @csrf
+                                    <button type="submit" class="btn btn-primary">Enviar</button>
+                                </div>
+
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <table id="indexUsers" class="table table-striped" style="width:100%">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Nombre</th>
+                                <th>Correo</th>
+                                <th>Acción</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($users as $user)
+                                <tr>
+                                    <td>{{ $user->id }}</td>
+                                    <td>{{ $user->name }}</td>
+                                    <td>{{ $user->email }}</td>
+                                    <td>
+                                        <form action="{{ route('users.destroy', $user) }}" method="POST">
+                                            @method('DELETE')
+                                            @csrf
+                                            <input
+                                                type="submit"
+                                                value="Eliminar"
+                                                class="btn btn-sm btn-danger"
+                                                onclick="return confirm('Desea eliminar el usuario?...');"/>
+                                        </form>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- JS Bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.24/js/dataTables.bootstrap5.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('#indexUsers').DataTable();
+        } );
+        $(function () {
+            $('[data-toggle="tooltip"]').tooltip()
+        })
+    </script>
+</body>
+</html>
